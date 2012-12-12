98e2b0ea295b6e28e2a4481f6c34b867ffbe51d4
Jason Pope <cowholio4@gmail.com>
2012-12-11 20:31:48 -0800

    first iteration just copying from google docs

---
+++ social_analytics.js
@@ -0,0 +1,42 @@
+// From https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingSocial#facebook
+
+FB.Event.subscribe('edge.create', function(targetUrl) {
+    _gaq.push(['_trackSocial', 'facebook', 'like', targetUrl]);
+});
+
+FB.Event.subscribe('edge.remove', function(targetUrl) {
+    _gaq.push(['_trackSocial', 'facebook', 'unlike', targetUrl]);
+});
+
+FB.Event.subscribe('message.send', function(targetUrl) {
+    _gaq.push(['_trackSocial', 'facebook', 'send', targetUrl]);
+});
+
+function trackTwitter(intent_event) {
+  if (intent_event) {
+    var opt_pagePath;
+    if (intent_event.target && intent_event.target.nodeName == 'IFRAME') {
+          opt_target = extractParamFromUri(intent_event.target.src, 'url');
+    }
+    _gaq.push(['_trackSocial', 'twitter', 'tweet', opt_pagePath]);
+  }
+}
+
+//Wrap event bindings - Wait for async js to load
+twttr.ready(function (twttr) {
+  //event bindings
+  twttr.events.bind('tweet', trackTwitter);
+});
+
+
+function extractParamFromUri(uri, paramName) {
+  if (!uri) {
+    return;
+  }
+  var regex = new RegExp('[\\?&#]' + paramName + '=([^&#]*)');
+  var params = regex.exec(uri);
+  if (params != null) {
+    return unescape(params[1]);
+  }
+  return;
+}
