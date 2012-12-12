848069f28e45ad3c36ffa530aec1b965cf2d7a4c
Jason Pope <cowholio4@gmail.com>
2012-12-12 08:26:59 -0800

    added more twitter events

--- social_analytics.js
+++ social_analytics.js
@@ -1,5 +1,10 @@
-// From https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingSocial#facebook
-window.fbAsyncInit = function() {
+// Track Social Actions to Google Analytics
+// Resources:
+//  https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingSocial#facebook
+//  https://dev.twitter.com/docs/intents/events
+
+/* Facebook */
+window.fbAsyncInitTracking = function() {
 
   FB.Event.subscribe('edge.create', function(targetUrl) {
       _gaq.push(['_trackSocial', 'facebook', 'like', targetUrl]);
@@ -14,6 +19,7 @@ window.fbAsyncInit = function() {
   });
 };
 
+/* Twitter */
 function track_tweet(intent_event) {
   if (intent_event) {
     var opt_pagePath;
@@ -28,13 +34,24 @@ function track_follow( event ) {
   var followed_user_id = event.data.user_id;
   var followed_screen_name = event.data.screen_name;
   _gaq.push(['_trackSocial', 'twitter', 'follow', followed_screen_name ]);
-
 }
+
+function track_retweet( event ) {
+  var retweeted_tweet_id = event.data.source_tweet_id;
+  _gaq.push(['_trackSocial', 'twitter', 'retweet', retweeted_tweet_id]);
+}
+function track_favorite( event ) {
+  var favorited_tweet_id = event.data.tweet_id;
+  _gaq.push(['_trackSocial', 'twitter', 'favorite', favorited_tweet_id]);
+}
+
 //Wrap event bindings - Wait for async js to load
 twttr.ready(function (twttr) {
   //event bindings
   twttr.events.bind('tweet', track_tweet);
   twttr.events.bind('follow', track_follow );
+  twttr.events.bind('retweet', track_retweet );
+  twttr.events.bind('favorite', track_favorite );
 
 });
 
